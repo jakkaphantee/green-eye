940591001540362599fe734efabcc76e357917eb
Jakkaphan Payuhakait <tee@dept.email>
2021-06-03 19:08:34 +0700

    add menu function

--- src/components/General/HamburgerMenu.vue
+++ src/components/General/HamburgerMenu.vue
@@ -9,7 +9,18 @@
       class="menu-container"
       :class="isMenuOpen ? 'show-menu' : ''"
     >
-      men
+      <div
+        v-for="(menu, index) in menuList"
+        :key="menu.imageName"
+        class="menu-section py-4"
+        :class="index === 4 ? '' : 'menu-border'"
+      >
+        <img
+          :class="menu.style"
+          :src="require(`@/assets/images/menu/${menu.imageName}.png`)"
+          @click="changePage(menu.routeName)"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -19,12 +30,44 @@ export default {
   name: 'HamburgerMenu',
   data: () => ({
     isMenuOpen: false,
-    menuIconName: 'menu_icon'
+    menuIconName: 'menu_icon',
+    menuList: [
+      {
+        imageName: 'menu_home',
+        routeName: 'LandingPage',
+        style: 'home-size'
+      },
+      {
+        imageName: 'menu_about',
+        routeName: 'About',
+        style: 'about-size'
+      },
+      {
+        imageName: 'menu_quiz',
+        routeName: 'Quiz',
+        style: 'quiz-size'
+      },
+      {
+        imageName: 'menu_shop',
+        routeName: 'LandingPage',
+        style: 'shop-size'
+      },
+      {
+        imageName: 'menu_contact',
+        routeName: 'LandingPage',
+        style: 'contact-size'
+      },
+    ]
   }),
   watch: {
     isMenuOpen(newValue) {
-      console.log('in')
       this.menuIconName = newValue ? 'close_menu' : 'menu_icon'
+    }
+  },
+  methods: {
+    changePage(routeName) {
+      this.$router.push({ name: routeName })
+      this.isMenuOpen = false
     }
   }
 }
@@ -51,7 +94,7 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  padding-top: 60px;
+  padding: 90px 25px 0px 25px;
   width: 100vw;
   height: 100vh;
   background-color: $app-background-color;
@@ -62,6 +105,34 @@ export default {
   &.show-menu {
     visibility: visible;
     opacity: 1;
+  }
+}
+.menu-section {
+  position: relative;
+  display: block;
+  width: 100%;
+  &.menu-border {
+    border-bottom: $app-border-color solid 0.025rem;
+  }
+  img {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .home-size {
+    width: 100px;
+  }
+  .about-size {
+    width: 110px;
+  }
+  .quiz-size {
+    width: 160px;
+  }
+  .shop-size {
+    width: 90px
+  }
+  .contact-size {
+    width: 140px;
   }
 }
 </style>

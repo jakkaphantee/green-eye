7ba09d98f58ce1c62eced5b03024b44fd1fd76a7
Jakkaphan Payuhakait <tee@dept.email>
2021-06-20 16:23:46 +0700

    add image carousel

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -7,10 +7,24 @@
       </div>
     </div>
     <div class="product-image-wrapper mt-4">
-      <img class="arrow-indicator left" :src="require('@/assets/images/general/arrow_left.png')" />
-      <img class="arrow-indicator right" :src="require('@/assets/images/general/arrow_right.png')" />
+      <img
+        class="arrow-indicator left"
+        :src="require('@/assets/images/general/arrow_left.png')"
+        @click="previousImage()"
+      />
+      <img
+        class="arrow-indicator right"
+        :src="require('@/assets/images/general/arrow_right.png')"
+        @click="nextImage()"
+      />
       <div class="product-image-box">
-        <img class="product-image" :src="require('@/assets/images/product/bergamot_01.jpeg')" />
+        <img
+          v-for="(imageName, index) in currentProduct.imageList"
+          :key="imageName"
+          class="product-image"
+          :class="checkActiveClass(index)"
+          :src="require(`@/assets/images/product/${imageName}.jpeg`)"
+        />
       </div>
     </div>
     <div class="product-image-index-container mt-2">
@@ -18,6 +32,7 @@
         v-for="index in totalImageCount"
         :key="index"
         class="product-image-marker"
+        :class="index - 1 === currentImageIndex ? 'active' : ''"
       />
     </div>
     <div class="product-detail-container mt-4">
@@ -86,7 +101,10 @@ export default {
         priceTag: '3.00$'
       }
     ],
-    productId: ''
+    productId: '',
+    currentImageIndex: 0,
+    currentTransition: '',
+    nextImageTimer: null
   }),
   computed: {
     currentProduct() {
@@ -95,11 +113,72 @@ export default {
     totalImageCount() {
       return this.currentProduct.imageList.length || 0
     },
+    prevIndex() {
+      if (this.currentImageIndex !== 0 && this.currentImageIndex < this.totalImageCount) {
+        return this.currentImageIndex - 1
+      } else {
+        return this.totalImageCount - 1
+      }
+    },
+    nextIndex() {
+      if (this.currentImageIndex < this.totalImageCount - 1) {
+        return this.currentImageIndex + 1
+      } else {
+        return 0
+      }
+    }
   },
   created() {
     this.productId = this.$route.params.id
+    this.setNextImageTimer()
   },
   methods: {
+    previousImage() {
+      this.currentTransition = 'prev'
+      this.setNextImageTimer()
+      if (this.currentImageIndex !== 0 && this.currentImageIndex < this.totalImageCount) {
+        this.currentImageIndex -= 1
+      } else {
+        this.currentImageIndex = this.totalImageCount - 1
+      }
+    },
+    nextImage() {
+      this.currentTransition = 'next'
+      this.setNextImageTimer()
+      if (this.currentImageIndex < this.totalImageCount - 1) {
+        this.currentImageIndex += 1
+      } else {
+        this.currentImageIndex = 0
+      }
+    },
+    checkActiveClass(imageIndex) {
+      switch (this.currentTransition) {
+        case 'prev': {
+          if (imageIndex === this.currentImageIndex) {
+            return 'prev-to-active'
+          }
+          if (imageIndex === this.nextIndex) {
+            return 'active-to-next'
+          }
+          return 'hide'
+        }
+        case 'next': {
+          if (imageIndex === this.currentImageIndex) {
+            return 'next-to-active'
+          }
+          if (imageIndex === this.prevIndex) {
+            return 'active-to-prev'
+          }
+          return 'hide'
+        }
+      }
+    },
+    setNextImageTimer() {
+      clearInterval(this.nextImageTimer)
+      this.nextImageTimer = setTimeout(() => {
+        this.nextImage()
+      }, 3000)
+    },
     addProductToCart() {
       // add to cart
     }
@@ -178,12 +257,70 @@ export default {
   height: calc(90vw - 40px);
   max-height: 350px;
   border: 1px solid $primary-color;
+  overflow: hidden;
   .product-image {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+    &.prev-to-active {
+      animation: prev-to-active .2s forwards;
+      z-index: 1;
+      @keyframes prev-to-active {
+        from {
+          left: -100%;
+        }
+        to {
+          left: 0;
+        }
+      }
+    }
+    &.next-to-active {
+      animation: next-to-active .2s forwards;
+      z-index: 1;
+      @keyframes next-to-active {
+        from {
+          left: 100%;
+        }
+        to {
+          left: 0;
+        }
+      }
+    }
+    &.active-to-next {
+      animation: active-to-next .2s forwards;
+      z-index: 0;
+      @keyframes active-to-next {
+        from {
+          left: 0;
+        }
+        to {
+          left: 100%;
+        }
+      }
+    }
+    &.active-to-prev {
+      animation: active-to-prev .2s forwards;
+      z-index: 0;
+      @keyframes active-to-prev {
+        from {
+          left: 0;
+        }
+        to {
+          left: -100%;
+        }
+      }
+    }
+    &.prev {
+      left: -100%;
+    }
+    &.next {
+      left: 100%;
+    }
+    &.hide {
+      opacity: 0;
+    }
   }
 }
 .product-image-index-container {
